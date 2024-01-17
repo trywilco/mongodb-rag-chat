e5fd91f29e6c79a0be8b5fa6a594f1338f81134c
NivRichter <nivrichter@gmail.com>
2024-01-17 13:22:41 +0200

    update docker compose up command

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -22,5 +22,5 @@ echo "printf \"\n\n☁️☁️☁️️ Anythink: Develop in the Cloud ☁️�
 echo "printf \"\n=============================================\n\"" >> ~/.bashrc
 echo "gh codespace ports -c $CODESPACE_NAME" >> ~/.bashrc
 echo "printf \"=============================================\n\"" >> ~/.bashrc
-echo "printf \"(Once docker-compose is up and running, you can access the frontend and backend using the above urls)\n\"" >> ~/.bashrc
-echo "printf \"\n\x1b[31m \x1b[1m👉 Type: \\\`docker-compose up\\\` to run the project. 👈\n\n\"" >> ~/.bashrc
+echo "printf \"(Once docker compose is up and running, you can access the frontend and backend using the above urls)\n\"" >> ~/.bashrc
+echo "printf \"\n\x1b[31m \x1b[1m👉 Type: \\\`docker compose up\\\` to run the project. 👈\n\n\"" >> ~/.bashrc
